fb3787d6dc0e69cadb2b62a8e874c65a56587822
jhosenthG <jhosenth@gmail.com>
2025-08-06 13:59:30 -0400

    **Change dependency**

--- app/models/room.py
+++ app/models/room.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from app.database import Base
+from app.core.database import Base
 
 
 class Room(Base):
