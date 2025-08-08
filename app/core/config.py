b415af8b6b8a7159a22eb93da51c8b98ca6879cb
jhosenthG <jhosenth@gmail.com>
2025-08-08 16:31:45 -0400

    Change in version of pydantic libraries

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,16 @@
-from pydantic.v1 import BaseSettings
-
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
-    model_config = {"env_file": ".venv", "extra": "allow" }
+    model_config = {
+        "env_file": ".env",
+        "env_file_encoding": "utf-8",
+        "extra": "allow"
+    }
+
 settings = Settings()
